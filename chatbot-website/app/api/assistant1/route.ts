d407168bb5661a35bcd06274157f7c664dfd95a0
BradJKim <bradjkim03@gmail.com>
2024-02-26 10:52:39 -0800

    file structure

--- chatbot-website/app/api/assistant1/route.ts
+++ chatbot-website/app/api/assistant1/route.ts
@@ -3,7 +3,6 @@ import OpenAI from "openai";
 import { Run } from "openai/resources/beta/threads/index.mjs";
 import { MessageContentText } from "openai/resources/beta/threads/messages/messages";
 
-// Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || "",
 });
