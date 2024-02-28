2553f810c0dd07be36586980f06eec5008676123
BradJKim <bradjkim03@gmail.com>
2024-02-27 21:11:56 -0800

    formatting and descriptions

--- chatbot-website/app/about/page.tsx
+++ chatbot-website/app/about/page.tsx
@@ -6,8 +6,29 @@ export default function About() {
 
     return (
         <main>
-            <div>
-                <p>This is the about  page.</p>
+            <div className="text-center mt-40 mb-10">
+                <p>
+                    This website is an example website meant to demonstrate how to the OpenAI Assistant API
+                </p>
+                <p>
+                    can be applied to a website to allow a user to interact with custom chat bots.
+                </p>
+            </div>
+            <div className="text-center mb-20">
+                <p> 
+                    The chatbots are designed to provide assistance to the user and will provide relavent 
+                </p>
+                <p> 
+                    information and/or advice as long as it is within OpenAI's guidelines.
+                </p>
+            </div>
+            <div className="flex justify-center">
+                <p className="px-2">
+                    Github Link: 
+                </p>
+                <div className=" font-bold to-blue-600">
+                    <a target="_blank" href="https://github.com/BradJKim/Chatbot-Website" rel="noopener noreferrer">https://github.com/BradJKim/Chatbot-Website</a>
+                </div>
             </div>
         </main>
       );
--- chatbot-website/app/page.tsx
+++ chatbot-website/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useRef } from "react";
 import { useRouter } from 'next/navigation';
 
 import Image, { StaticImageData } from 'next/image'
@@ -45,13 +44,17 @@ export default function Home() {
     const router = useRouter();
 
     return (
-        <main>
+        <main className="flex flex-col justify-center">
             <div>
-                <h1>Welcome to My Next.js App</h1>
-                <p>This is the default home page.</p>
+                <h1 className="py-4 mt-40 text-center">
+                    Welcome to my Chatbot App built with Next.js
+                </h1>
+                <p className="py-4 text-center">
+                    Choose one of the chatbots below to interact with
+                </p>
             </div>
 
-            <div className="flexbox-container">
+            <div className="flexbox-container justify-evenly py-16 px-25">
                 <Chatbot onClick={() => router.push('/chatbots/bot1')} text="Chatbot1" image={Bot1Image}/>
                 <Chatbot onClick={() => router.push('/chatbots/bot2')} text="Chatbot2" image={Bot2Image}/>
                 <Chatbot onClick={() => router.push('/chatbots/bot3')} text="Chatbot3" image={Bot3Image}/>
@@ -59,15 +62,16 @@ export default function Home() {
                 <Chatbot onClick={() => router.push('/chatbots/bot5')} text="Chatbot5" image={Bot5Image}/> 
             </div>
 
-            <div>
+            <div className='flex justify-center py-6'>
                 <button 
                     onClick={() => router.push('/about')}
                     style={{
                         padding: '10px 20px', // Adjust padding for size
                         border: '2px solid #ccc', // Border style
                         borderRadius: '5px', // Border radius for rounded corners
+                        marginTop: '2.5rem',
                     }}>
-                    About
+                    About Page
                 </button>
             </div>
         </main>
